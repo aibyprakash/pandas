b7487391f817a381c986c7afbf8a90b8b31fcdb9
aibyprakash <muraliprakashmv@hotmail.com>
2025-05-15 15:39:58 +0400

    read files using pandas
    read file headers and specific locations from the file

--- intro.py
+++ intro.py
@@ -12,6 +12,32 @@ df_txt = pd.read_csv('pokemon_data.txt',delimiter='\t')
 
 #Read Headers of the file
 
-print(df.columns)
+#print(df.columns)
+
+#Read Each Column for example select names of the pokemon
+
+#print(df['Name','Type1','HP'])
+#print(df.Name)
+
+
+'''READ EACH ROW'''
+#print(df.head(4))
+#print(df.iloc[1])
+#print(df.iloc[0:4])
+'''
+for index,row in df.iterrows():
+    print(index,row['Name'])
+
+'''
+#print(df.loc[df['Type 1'] == 'Fire'])
+
+'''Read a specific location (R,C)'''
+
+
+print(df.iloc[2,1])
+
+
+
+
 
 
