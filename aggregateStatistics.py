cea4929ee68c1245a8088c05d3a2da1fa93ff21a
aibyprakash <muraliprakashmv@hotmail.com>
2025-05-19 14:32:56 +0400

    df.mean()
    dataframe mean calculation and operations

--- aggregateStatistics.py
+++ aggregateStatistics.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import re
 
 df = pd.read_csv('modified.csv')
 
-df.groupby(['Type 1']).mean()
-print(df)
+dfg=df.groupby(['Type 1']).mean(numeric_only=True).sort_values('Defense',ascending=False)
+print(dfg)
